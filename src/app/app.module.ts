1dc0ef083adccf9539028f99eab3b3872d37d2cd
ONYX Web Design Studio <berez95@mail.ru>
2017-04-23 15:50:16 +0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { routes } from './app.router';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { AdministrationComponent } from './administration/administration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ConfigurationComponent,
+    AdministrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routes
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
