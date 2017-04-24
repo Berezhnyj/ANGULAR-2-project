529ecff35a1d3819e056348bfeeaa3a3e9fdad5b
ONYX Web Design Studio <berez95@mail.ru>
2017-04-24 14:58:44 +0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import {TranslateModule} from '@ngx-translate/core';
+import {HttpModule, Http} from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { TranslateModule, MissingTranslationHandler } from 'ng2-translate';
+//import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 
 import { routes } from './app.router';
@@ -11,8 +13,11 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
 import { AdministrationComponent } from './administration/administration.component';
+import { MyMissingTranslationHandler } from './missingtemplate.component'
 
-
+/*export function HttpLoaderFactory(http: Http) {
+    return new TranslateHttpLoader(http);
+}*/
 
 @NgModule({
   declarations: [
@@ -21,14 +26,21 @@ import { AdministrationComponent } from './administration/administration.compone
     HeaderComponent,
     ConfigurationComponent,
     AdministrationComponent
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routes
+    routes,
+    TranslateModule.forRoot()
   ],
-  providers: [],
+  exports: [
+        TranslateModule
+    ],
+  providers: [
+      { provide: MissingTranslationHandler, useClass: MyMissingTranslationHandler}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
